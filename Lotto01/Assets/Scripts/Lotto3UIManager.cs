ffcfe1abf41826058a8924a8f84d773d0c5b07dd
Molkang <mitier26@gmail.com>
2023-01-06 23:43:53 +0900

    Lotto

--- Lotto01/Assets/Scripts/Lotto3UIManager.cs
+++ Lotto01/Assets/Scripts/Lotto3UIManager.cs
@@ -11,8 +11,13 @@ public class Lotto3UIManager : MonoBehaviour
     public Transform parent;
     public GameObject[] balls;
     public int[] numbers;
+
     private void OnEnable()
     {
+        // 렉의 주범
+        // 공이 Collecter 에 들어 갈 때 리스트에 저장하는 것으로 바꾸어야 할듯
+        // Collecter 가 한 몸이 아니고 각자 인것이 문제
+        
         balls = GameObject.FindGameObjectsWithTag("Selected");
 
         //for(int i = 0; i < balls.Length; i++)
